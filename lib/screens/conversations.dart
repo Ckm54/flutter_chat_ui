cf8dbdb9afc4ca66116b36ee9e787eaa0bcc00a0
collo <mutaic84@gmail.com>
2022-11-30 16:23:54 +0300

    :recycle: Refactor status ui

--- lib/screens/conversations.dart
+++ lib/screens/conversations.dart
@@ -1,3 +1,4 @@
+import 'package:chat_ui/screens/user_stories.dart';
 import 'package:flutter/material.dart';
 
 class Conversations extends StatefulWidget {
@@ -76,7 +77,7 @@ class _ConversationsState extends State<Conversations> {
                   const SizedBox(
                     height: 20,
                   ),
-                  _stories(),
+                  const UserStories(),
                 ],
               ),
             ),
@@ -87,173 +88,5 @@ class _ConversationsState extends State<Conversations> {
 }
 
 
-Widget _stories() {
-  return SingleChildScrollView(
-    scrollDirection: Axis.horizontal,
-    child: Row(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(right: 20),
-          child: Column(
-            children: <Widget>[
-              Container(
-                width: 60,
-                height: 60,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Color(0xFFe9eaec)
-                ),
-                child: const Center(
-                  child: Icon(
-                    Icons.add,
-                    size: 33,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const SizedBox(
-                width: 75,
-                child: Align(
-                  child: Text(
-                    'Your Story',
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
 
-        Row(
-          children: List.generate(storyList.length, (index) {
-            return Padding(
-              padding: const EdgeInsets.only(right: 20),
-              child: Column(
-                children: <Widget>[
-                  SizedBox(
-                    width: 60,
-                    height: 60,
-                    child: Stack(
-                      children: <Widget>[
-                        storyList[index]['hasStory']
-                        ? Container(
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              color: Colors.blueAccent,
-                              width: 3,
-                            )
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(3.0),
-                            child: Container(
-                              width: 75,
-                              height: 75,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                image: DecorationImage(
-                                  image: NetworkImage(
-                                      storyList[index]['imageUrl']
-                                  ),
-                                  fit: BoxFit.cover,
-                                )
-                              ),
-                            ),
-                          ),
-                        )
-                        : Container(
-                          width: 70,
-                          height: 70,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            image: DecorationImage(
-                              image: NetworkImage(
-                                storyList[index]['imageUrl']
-                              ),
-                              fit: BoxFit.cover,
-                            )
-                          ),
-                        ),
-                        storyList[index]['isOnline']
-                        ? Positioned(
-                          top: 38,
-                          left: 42,
-                          child: Container(
-                            width: 20,
-                            height: 20,
-                            decoration: BoxDecoration(
-                              color: const Color(0xFF66BB6A),
-                              shape: BoxShape.circle,
-                              border: Border.all(
-                                color: const Color(0xFFFFFFFF),
-                                width: 3
-                              )
-                            ),
-                          ),
-                        )
-                            : Container()
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    width: 75,
-                    child: Align(
-                      child: Text(
-                        storyList[index]['name'],
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            );
-          }),
-        )
-      ],
-    ),
-  );
-}
-
-
-
-List storyList = [
-  {
-    "name": "Novac",
-    "imageUrl": "https://randomuser.me/api/portraits/men/31.jpg",
-    "isOnline": true,
-    "hasStory": true,
-  },
-  {
-    "name": "Derick",
-    "imageUrl": "https://randomuser.me/api/portraits/men/81.jpg",
-    "isOnline": false,
-    "hasStory": false,
-  },
-  {
-    "name": "Mevis",
-    "imageUrl": "https://randomuser.me/api/portraits/women/49.jpg",
-    "isOnline": true,
-    "hasStory": false,
-  },
-  {
-    "name": "Emannual",
-    "imageUrl": "https://randomuser.me/api/portraits/men/35.jpg",
-    "isOnline": true,
-    "hasStory": true,
-  },
-  {
-    "name": "Gracy",
-    "imageUrl": "https://randomuser.me/api/portraits/women/56.jpg",
-    "isOnline": false,
-    "hasStory": false,
-  },
-  {
-    "name": "Robert",
-    "imageUrl": "https://randomuser.me/api/portraits/men/36.jpg",
-    "isOnline": false,
-    "hasStory": true,
-  }
-];
 
